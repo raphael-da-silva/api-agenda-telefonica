532c836c3ca4c13215329a504622c4a817ecb4c5
Raphael da Silva <raphael_correa_silva@hotmail.com>
2024-03-21 16:25:03 -0300

    Criação do html da view inicial

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,3 +1,33 @@
-<html>
-    Teste API Telefonica - Raphael da Silva
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Teste API Telefonica - Raphael da Silva</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <style>
+        .container{
+            max-width: 700px;
+        }
+    </style>    
+</head>
+
+<body class="bg-dark">
+    <div class="container bg-white p-2 text-center">
+        <h3 class="fw-bold">
+            Teste API (Agenda Telefonica) - Raphael da Silva
+        </h3>
+
+        <hr>
+
+        <div class="alert alert-info">
+            <a href="https://github.com/raphael-da-silva">Acessar meu github</a>
+        </div>
+
+        <div class="alert alert-warning">
+            A documentação da API está no README do repositório no Github.
+        </div>
+    </div>
+</body>
 </html>
